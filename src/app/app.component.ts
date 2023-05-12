2f62d8b6fe2958e9f1066d21b9ce261eeb8a077b
Savage Coder <savagecoder352@gmail.com>
2023-05-12 02:20:31 -0400

    Move to standalone component add app header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import {HeaderComponent} from "./components/header/header.component";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.less'],
+    standalone: true,
+    imports: [HeaderComponent]
 })
 export class AppComponent {
-  title = 'jalen-bracey';
+
 }
